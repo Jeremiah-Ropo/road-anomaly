7ee08e905adad26b3e91479e9ebefaac21fe5cc1
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2024-08-20 10:19:01 +0100

    chore: include cors

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 from app.src import create_app
 load_dotenv()
 
@@ -8,6 +9,7 @@ debug_mode = os.getenv("DEBUG")
 
 
 app = create_app()
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=debug_mode)
